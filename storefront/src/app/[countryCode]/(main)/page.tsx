8f5b97debae97d8458135ecb9780a0017388fe3d
Héctor Arranz <hectarra@ucm.es>
2025-04-06 19:09:39 +0200

    Volviendoa los origenes :(

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -1,13 +1,9 @@
 import { Metadata } from "next"
-import { builder } from "@builder.io/sdk";
-import { RenderBuilderContent } from "@modules/common/components/builder";
-
 
 import FeaturedProducts from "@modules/home/components/featured-products"
+import Hero from "@modules/home/components/hero"
 import { getCollectionsWithProducts } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
-
-builder.init("e2681541300e4e2d84c04a77559a707");
 
 export const metadata: Metadata = {
   title: "Medusa Next.js Starter Template",
@@ -27,17 +23,9 @@ export default async function Home({
     return null
   }
 
-  /* const content = await builder
-  .get("page", {
-    userAttributes: { urlPath: "/" },
-    options: { countryCode },
-    locale: countryCode,
-    prerender: false,
-  })
-  .toPromise(); */
-
   return (
     <>
+      <Hero />
       <div className="py-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
@@ -46,4 +34,3 @@ export default async function Home({
     </>
   )
 }
-
