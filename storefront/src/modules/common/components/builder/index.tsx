19f0e808400d1ac06c6a6f648444bea1405daadb
Héctor Arranz <hectarra@ucm.es>
2025-04-06 18:24:35 +0200

    Integración Builder.io

--- storefront/src/modules/common/components/builder/index.tsx
+++ storefront/src/modules/common/components/builder/index.tsx
@@ -7,7 +7,7 @@ import DefaultErrorPage from "next/error";
 type BuilderPageProps = ComponentProps<typeof BuilderComponent>;
 
 // Replace with your Builder.io Public API Key
-builder.init("70960fd039784ac08e7a9e0355f38349");
+builder.init("e2681541300e4e2d84c04a77559a707c");
 
 export function RenderBuilderContent(props: BuilderPageProps) { 
   const isPreviewing = useIsPreviewing(); 
